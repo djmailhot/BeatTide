5757994a3606f393cec7acf6eb29219a8dda9b45
Alex Miller <alexmiller@gmail.com>
2012-02-05 18:28:55 -0800

    Initialized the Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeatTide::Application.config.secret_token = '20f24937fce4777fb593925b743c9725befa92fdb1a6390c1753eaff76e2428825fb8472bf66e0e3bee07cd6a1f2d8492d840c4ad5a92a0e5eaab45fcd626953'
