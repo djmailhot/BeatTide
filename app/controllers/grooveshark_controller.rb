498dc42f485f4e4c27391fbe0e836d51d1d02774
Alex Miller <alexmiller@gmail.com>
2012-02-09 16:36:02 -0800

    grooveshark play song view

--- app/controllers/grooveshark_controller.rb
+++ app/controllers/grooveshark_controller.rb
@@ -2,11 +2,12 @@ class GroovesharkController < ApplicationController
   def getInfo
     client = Grooveshark::Client.new
     session = client.session
-    render :text => session
-    songs = client.search_songs('Nirvana')
-    songs.each do |s|
-      s.name
-    end
+    song = client.get_song_url_by_id(2607986)
+    render :text => song
+    
+    # resp = get_stream_auth by songid(song_id)
+    # resp['stream_key']
+    # pass this to the APIPlayer swf 
   end
 
   def findSong
