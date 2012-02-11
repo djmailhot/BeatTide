51bd662fdd1201f33a370a209211028cfad13b98
Alex Miller <alexmiller@gmail.com>
2012-02-11 14:50:25 -0800

    code cleanup

--- app/controllers/grooveshark_controller.rb
+++ app/controllers/grooveshark_controller.rb
@@ -11,12 +11,11 @@ class GroovesharkController < ApplicationController
   end
 
   def searchSong
-    #api_key = "4539534e418cc829a9a2d0c590573a25"
     api_key = "d44e54b31d4333b5940119c69fddc429"
     query = URI.escape(params[:query])
     url = URI.parse("http://tinysong.com/s/#{query}?format=json&limit=10&key=#{api_key}")
     response = Net::HTTP.get_response(url).body
-    render :text => response
+    render :json => response
   end
 
   def playSong
