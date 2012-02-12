73fe5a79d7d8ff1ea15a1e118267c51ac0a8369f
Alex Miller <alexmiller@gmail.com>
2012-02-12 13:32:15 -0800

    reorganized song views

--- app/controllers/grooveshark_controller.rb
+++ app/controllers/grooveshark_controller.rb
@@ -42,7 +42,7 @@ class GroovesharkController < ApplicationController
       url = URI.parse("#{TINY_SONG_API}/s/#{query}?format=json&limit=#{NUM_SEARCH_RESULTS}&key=#{TINY_SONG_API_KEY}")
       response = Net::HTTP.get_response(url).body
       @song_results = ActiveSupport::JSON.decode(response)
-      render :layout => false
+      render 'songs/song_list', :layout => false
     else
       render :text => "No query."
     end
