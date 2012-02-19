d7edae334aa2bcf499add5f35af93633a39b9d34
David Mailhot <djmailhot@gmail.com>
2012-02-19 13:01:21 -0800

    Added song, post rspec factories, linked javascript gem

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,27 @@
-# Generate a default user with valid characteristics
+# Factories for generating valid model objects
 #
-# Author:: Melissa Winstanley
+# Included factories:
+#   user
+#   song
+#   post
+#
 FactoryGirl.define do
+
+  # Generate a default user with valid characteristics
   factory :user do
     first_name   'Melissa'
     last_name    'Winstanley'
     username     'mwinst'
     facebook_id  619716339
+  end
+
+  factory :song do
+    api_id 42
+    title "The Last of the Wilds"
+  end
+
+  factory :post do
+    song 
+    user
   end
 end
