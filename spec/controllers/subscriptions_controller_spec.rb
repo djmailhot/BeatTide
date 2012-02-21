05fe778a2a3a61dc4dd283ed7cb9b6d0dae5d074
Tyler Rigsby <tjrigs@gmail.com>
2012-02-21 00:00:13 -0800

    changes from CRs

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -3,70 +3,125 @@ require 'spec_helper'
 # White box test of Subscriptions Controller
 # Author:: Tyler Rigsby
 describe SubscriptionsController do
-  render_views
+  
+  describe "authorization" do
+    it "shouldn't create subscription if not signed in" do
+      post :create
+      response.should redirect_to(signin_path)
+    end
+    
+    it "shouldn't delete if not signed in" do
+      delete :destroy, :id => 1
+      response.should redirect_to(signin_path)
+    end
+  end
 
   # Add subscription
   describe "POST 'create'" do
     before(:each) do
-      @user = User.find(test_sign_in(FactoryGirl.create(:user, :facebook_id => Factory.next(:facebook_id))))
-      @subscribed = FactoryGirl.create(:user, :id => 2, :facebook_id => Factory.next(:facebook_id))
-      @subscribed.save
+      @user = FactoryGirl.create(:user)
+      @subscribed = FactoryGirl.create(:user)
+      test_sign_in(@user)
     end
 
-    it "should add a subscription to subscriber" do
-      post :create, :subscription => {:subscribed_id => @subscribed.id }
-
-      @user.subscriptions.length.should eq(1)
+    describe "success" do
+      it "should add a subscription to subscriber" do
+        post :create, :subscription => { :subscribed_id => @subscribed.id }
+        
+        @user.subscriptions.length.should eq(1)
+      end
+      
+      it "should add a subscriber to subscribed" do
+        post :create, :subscription => { :subscribed_id => @subscribed.id }
+        
+        @user.subscribers.length.should eq(1)
+      end
+      
+      it "should redirect to the subscribed's page" do
+        post :create, :subscription => { :subscribed_id => @subscribed.id }
+        
+        response.should redirect_to(@subscribed)
+      end
     end
 
-    it "should add a subscriber to subscribed" do
-      post :create, :subscription => {:subscribed_id => @subscribed.id }
+    describe "failure" do
+      it "subscriber shouldn't get subscription on self-subscribe" do
+        post :create, :subscription => { :subscribed_id => @user.id }
+        
+        @user.subscriptions.length.should eq(0)
+      end
+
+      it "subscriber shouldn't get subscribed on self-subscribe" do
+        post :create, :subscription => { :subscribed_id => @user.id }
+        
+        @user.subscribers.length.should eq(0)
+      end
+
+      it "should error on subscribe to non-existent user" do
+        post :create, :subscription => { :subscribed_id => 50 }
+
+        flash.now[:error].should =~ /user doesn't exist/i
+      end
+
+      it "should error on self-subscribe" do
+        post :create, :subscription => { :subscribed_id => @user.id }
+        
+        flash.now[:error].should =~ /self subscribe/i
+      end
       
-      @user.subscribers.length.should eq(1)
-    end
-    
-    it "should redirect to the subscribed's page" do
-      post :create, :subscription => {:subscribed_id => @subscribed.id }
-      
-      response.should redirect_to(@subscribed)
-    end
-    
-    it "should fail for self-subscribes" do
-      post :create, :subscription => {:subscribed_id => @user.id }
-      
-      @user.subscriptions.length.should eq(0)
-      @user.subscribers.length.should eq(0)
-    end
-    
-    it "shouldn't allow multi-subscribes" do
-      post :create, :subscription => {:subscribed_id => @subscribed.id }
-      post :create, :subscription => {:subscribed_id => @subscribed.id }
-      
-      @user.subscriptions.length.should eq(1)
-      @user.subscribers.length.should eq(1)
+      it "subscriber shouldn't get two subscriptions to same subscribed" do
+        post :create, :subscription => { :subscribed_id => @subscribed.id }
+        post :create, :subscription => { :subscribed_id => @subscribed.id }
+        
+        @user.subscriptions.length.should eq(1)
+      end
+
+      it "subscribed shouldn't get two subscibers from same subscription" do
+        post :create, :subscription => { :subscribed_id => @subscribed.id }
+        post :create, :subscription => { :subscribed_id => @subscribed.id }
+        
+        @user.subscribers.length.should eq(1)
+      end
+
+      it "should error on multi-subscribe" do
+        post :create, :subscription => { :subscribed_id => @subscribed.id } 
+        post :create, :subscription => { :subscribed_id => @subscribed.id }
+        
+        flash.now[:error].should =~ /already subscribed/i
+      end
     end
   end
 
   # delete subscription
   describe "DELETE 'destroy'" do
     before (:each) do
-      @subscriber = User.find(test_sign_in(FactoryGirl.create(:user, :facebook_id => Factory.next(:facebook_id))))
-      @subscribed = FactoryGirl.create(:user, :id => 2, :facebook_id => Factory.next(:facebook_id))
-      post :create, :subscription => {:subscribed_id => @subscribed.id }
+      @subscriber = FactoryGirl.create(:user)
+      test_sign_in(@subscriber)
+      @subscribed = FactoryGirl.create(:user)
+
+      post :create, :subscription => { :subscribed_id => @subscribed.id }
       @subscription = @subscriber.subscriptions[0]
     end
   
-    it "should remove the subscription" do
-      delete :destroy, :id => @subscription.id
-
-      Subscription.all.length.should eq(0)
-      @subscriber.subscriptions.length.should eq(0)
+    describe "success" do
+      it "should remove the subscription" do
+        delete :destroy, :id => @subscription.id
+        
+        Subscription.all.length.should eq(0)
+        @subscriber.subscriptions.length.should eq(0)
+      end
+      
+      it "should redirect to the subscribed's page" do
+        delete :destroy, :id => @subscription
+        response.should redirect_to(@subscribed)
+      end
     end
-
-    it "should redirect to the subscribed's page" do
-      delete :destroy, :id => @subscription
-      response.should redirect_to(@subscribed)
+    
+    describe "failure" do
+      it "should error on attempting to delete nonexistent subscription" do
+        delete :destroy, :id => 50
+        flash.now[:error].should =~ /not subscribed/i
+      end
     end
   end  
 end
-  
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,21 +1,31 @@
-# Generate a default user with valid characteristics
+# Factories for generating valid model objects
 #
-# Author:: Melissa Winstanley
+# Included factories:
+#   user
+#   song
+#   post
+#
 FactoryGirl.define do
+
+  # Generate a user with a unique username and facebook id 
+  # with each call to the factory
   factory :user do
     first_name   'Melissa'
     last_name    'Winstanley'
-    username     'mwinst'
-    facebook_id  619716339
+    sequence(:username)     { |n| "mwinst#{n}" }
+    sequence(:facebook_id)  { |n| 619716339 + n }
+  end
+
+  # Generate a song with a unique api id
+  # with each call to the factory
+  factory :song do
+    sequence(:api_id)  { |n| 42 + n }
+    title              "I Want You Now"
+  end
+
+  # Generate a post, with song and user attributes parameterized
+  factory :post do
+    song
+    user
   end
 end
-
-Factory.sequence :username do |n|
-  "user#{n}"
-end
-
-Factory.sequence :facebook_id do |n|
-  n
-end
-
-
