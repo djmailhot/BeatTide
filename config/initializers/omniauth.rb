6ea5c815b595a25a35807f9d82bbd7baabcdd260
Melissa Winstanley <mwinst@cs.washington.edu>
2012-03-04 17:20:19 -0800

    Switched Facebook Developer key to BeatTide V1 website

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :facebook, '228087167287078', 'd65b5e113f1d7bb45877467fbfaa8357',
+    provider :facebook, '235268933224088', '1cdc768cc3c718127f7ce5ac335c52c1',
     {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
   elsif Rails.env.development?
     provider :facebook, '323366737699173', '708bec06393c1ad2c5c48d81c1a2ae09',
