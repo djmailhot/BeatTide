113e1a69bdab0773b512c8ad9fb2d2f6d6c6e75e
bkw7 <brettw07@gmail.com>
2012-03-06 13:28:16 -0800

    Added FAQ, Tutorial, and About pages

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,9 +5,11 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :facebook, '235268933224088', '1cdc768cc3c718127f7ce5ac335c52c1'
+    provider :facebook, '235268933224088', '1cdc768cc3c718127f7ce5ac335c52c1',
+    {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
   elsif Rails.env.development?
-    provider :facebook, '323366737699173', '708bec06393c1ad2c5c48d81c1a2ae09'
+    provider :facebook, '323366737699173', '708bec06393c1ad2c5c48d81c1a2ae09',
+    {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
   else
     provider :facebook, '323366737699173', '708bec06393c1ad2c5c48d81c1a2ae09'
   end
