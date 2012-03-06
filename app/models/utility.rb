7b5a66d5560d739c1a8725279fc813769cc5e70c
Melissa Winstanley <mwinst@cs.washington.edu>
2012-03-06 12:52:42 -0800

    Added comments to Utility module

--- app/models/utility.rb
+++ app/models/utility.rb
@@ -1,3 +1,5 @@
+# Provides utility functions for dealing with input validation
+# Author:: Melissa Winstanley
 module Utility
   module_function
 
@@ -10,6 +12,9 @@ module Utility
                      auth["info"]["last_name"].blank?)
   end
 
+  # Checks that the given string is no longer than the given length. If
+  # it is longer, returns the string truncated to the given length;
+  # otherwise returns the orginal string
   def check_length(str, length)
     if !str.nil? && str.length > length
       str = str[0,length]
