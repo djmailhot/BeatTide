de7f79ed9796414d847eebb4969990a139ea3a4b
bkw7 <brettw07@gmail.com>
2012-03-09 03:02:24 -0800

    Added indexes to database

--- db/migrate/20120210021647_create_subscriptions.rb
+++ db/migrate/20120210021647_create_subscriptions.rb
@@ -1,7 +1,7 @@
 # The table "subscriptions", created by this class keeps track of the user ids
 # of each of the subscribers and subscribed users
 #
-# Author: Melissa Winstanley, David Mailhot, Tyler Rigsby
+# Author: Tyler Rigsby
 class CreateSubscriptions < ActiveRecord::Migration
   def change
     create_table :subscriptions do |t|
