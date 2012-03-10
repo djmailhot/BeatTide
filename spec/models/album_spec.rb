1c773747ac980d89d2bfea1a58829ca1d3239f33
Melissa Winstanley <mwinst@cs.washington.edu>
2012-03-10 11:34:32 -0800

    Updated model specs, cleaned up comments, fixed models in accordance with specs

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,63 +1,14 @@
 require 'spec_helper'
+require 'song_metadata_helper'
 
-# Test the Album model. White-box test.
-# Author:: Brett Webber
+# Test the Album model. Black-box test.
+# Author:: Brett Webber, Melissa Winstanley
 describe Album do
-  def new_album(params)
-    album = Album.new
-    album.title = params[:title]
-    album.api_id = params[:api_id]
-    album
-  end
 
-  before(:each) do
-    @attr = { :title => "My album", :api_id => 10 }
-  end
+  # Check title and API ID validations.
+  it_behaves_like "validating metadata"
 
-  describe "testing validations" do
-    it "should validate correct paramaters" do
-      album_valid = new_album(@attr)
-      album_valid.should be_valid
-    end
+  # Check finding and creating.
+  it_behaves_like "metadata with finding"
 
-    # Should not allow an album to be created with an empty name
-    it "should not allow empty name" do
-      album_no_name = new_album(@attr.merge(:title => "" ))
-      album_no_name.should_not be_valid
-    end
-
-    # Tries to make an album with a name of more than 200 characters
-    it "should allow names of greater than 200 characters" do
-      album = new_album(@attr.merge(:title => "k" * 201))
-      album.should_not be_valid
-    end
-
-    # Tries to create two albums with the same name
-    it "should not allow duplicate album API IDs" do
-      album = FactoryGirl.create(:album)
-      secondary_album = new_album(@attr.merge(:api_id => album.api_id))
-      secondary_album.should_not be_valid
-    end
-
-    it "should not allow nil album API IDs" do
-      album = new_album(@attr.merge(:api_id => nil))
-      album.should_not be_valid
-    end
-
-    it "should not allow negative album API IDs" do
-      album = new_album(@attr.merge(:api_id => -10))
-      album.should_not be_valid
-    end
-
-    it "should not allow non-integer album API IDs" do
-      album = new_album(@attr.merge(:api_id => "hello"))
-      album.should_not be_valid
-    end
-
-    # Should not allow an album to be created with a nil name
-    it "should not allow nil name" do
-      album_nil_name = new_album(@attr.merge(:title => nil))
-      album_nil_name.should_not be_valid
-    end
-  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,82 +1,25 @@
 require 'spec_helper'
+require 'song_metadata_helper'
+require 'likes_helper'
 
-# Test the Song model. White-box test.
-# Author:: Brett Webber
+# Test the Song model. Black-box test.
+# Author:: Brett Webber, Melissa Winstanley
 describe Song do
-
-  def new_song(params)
-    song = Song.new
-    song.title = params[:title]
-    song.api_id = params[:api_id]
-    song
-  end
 
   # Sets up the creation parameters for a Song
   before(:each) do
-    @attr = {
+    @params = {
+      :class => Song,
       :title => "Example Title",
       :api_id => "123"
     }
   end
 
-  # Songs should be created with likes intialized to 0
-  it "should create songs with likes set to 0" do
-    song = FactoryGirl.create(:song)
-    song.like_count.should eq(0)
-  end
+  # Check title and API ID validations.
+  it_behaves_like "validating metadata"
 
-  # api_ids should be unique, so there should not be multiple songs with the same api_id
-  it "should allow duplicate titles" do
-    song_original = FactoryGirl.create(:song)
-    song_duplicate_title = new_song(@attr.merge(:title => song_original.title))
-    song_duplicate_title.should be_valid
-  end
-
-  # A Song should not be able to be created with a title that is the empty string
-  it "should not allow empty title" do
-    song_no_title = new_song(@attr.merge(:title => ""))
-    song_no_title.should_not be_valid
-  end
-
-  # A Song should not be able to be created with a title that consists only of whitespace
-  it "should not allow empty title" do
-    song_no_title = new_song(@attr.merge(:title => "      "))
-    song_no_title.should_not be_valid
-  end
-
-  # A Song should not be able to be created with a title that is nil
-  it "should not allow nil title" do
-    song_nil_title = new_song(@attr.merge(:title => nil))
-    song_nil_title.should_not be_valid
-  end
-
-  # A Song should not be able to be created with a nil api_id
-  it "should not allow empty api_id" do
-    song_no_api_id = new_song(@attr.merge(:api_id => nil))
-    song_no_api_id.should_not be_valid
-  end
-
-  # A Song should be allowed to be created with the same title as another Song
-  it "should not allow duplicate API IDs" do
-    song_original = FactoryGirl.create(:song)
-    song_duplicate_id = new_song(@attr.merge(:api_id => song_original.api_id))
-    song_duplicate_id.should_not be_valid
-  end
-
-  # Liking once should increase the likes attribute by one
-  it "should increase likes by one when liked" do
-    song = FactoryGirl.create(:song)
-    song.like!
-    song.like_count.should eq(1)
-  end
-
-  # Liking multiple times should increase the likes attribute by the amount of times that it was liked
-  it "should increase likes when liked multiple times" do
-    song = FactoryGirl.create(:song)
-    song.like!
-    song.like!
-    song.like!
-    song.like!
-    song.like_count.should eq(4)
+  # Check liking behavior
+  it_should_behave_like "likeable" do
+    let(:likeable) { FactoryGirl.create(:song) }
   end
 end
